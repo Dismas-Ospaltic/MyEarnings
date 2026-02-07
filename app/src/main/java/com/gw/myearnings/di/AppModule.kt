ee67acda824c563eaf345519c6f4d573bd2ed108
Dismas-Ospaltic <kizaidismas@gmail.com>
2026-02-07 17:21:15 +0300

    dependency Injection setup

---
+++ app/src/main/java/com/gw/myearnings/di/AppModule.kt
@@ -0,0 +1,21 @@
+package com.gw.myearnings.di
+
+
+
+
+import com.gw.myearnings.data.local_storage.EarnedCashDataBase
+import com.gw.myearnings.repository.EarnedCashRepository
+import com.gw.myearnings.viewmodel.EarnedCashViewModel
+import org.koin.dsl.module
+import org.koin.androidx.viewmodel.dsl.viewModel
+
+
+val appModule = module {
+//  Define dependency injection here
+
+    single { EarnedCashDataBase.getDatabase(get()).earnedCashDao() }
+    single { EarnedCashRepository(get()) }
+    viewModel{ EarnedCashViewModel(get())}
+
+
+}
