a83ac2c1bf6f1d792dee3f85929b7c70fa7c008c
Dismas-Ospaltic <kizaidismas@gmail.com>
2026-02-11 14:14:17 +0300

    More Ui refinment

--- app/src/main/java/com/gw/myearnings/app_screens/ui_components/AddEarnings.kt
+++ app/src/main/java/com/gw/myearnings/app_screens/ui_components/AddEarnings.kt
@@ -3,6 +3,9 @@ package com.gw.myearnings.app_screens.ui_components
 
 
 import android.widget.Toast
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -32,6 +35,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
@@ -277,6 +281,15 @@ fun AddEarnings(
 
     val context = LocalContext.current
 
+
+    val interactionSource = remember { MutableInteractionSource() }
+    val pressed by interactionSource.collectIsPressedAsState()
+
+    val scale by animateFloatAsState(
+        targetValue = if (pressed) 0.96f else 1f,
+        label = ""
+    )
+
     // Error states
     var amountSavedError by remember { mutableStateOf(false) }
     var amountEarnedError by remember { mutableStateOf(false) }
@@ -327,12 +340,16 @@ fun AddEarnings(
                     label = { Text("Source *") },
                     modifier = Modifier.fillMaxWidth(),
                     singleLine = true,
+                    isError = sourceError,
                     colors = OutlinedTextFieldDefaults.colors(
                         focusedBorderColor = backgroundColor,
                         focusedLabelColor = backgroundColor,
                         cursorColor = backgroundColor
                     )
                 )
+                if (sourceError) {
+                    Text("Enter source", color = Color.Red, fontSize = 12.sp)
+                }
 
                 // AMOUNT EARNED
                 OutlinedTextField(
@@ -423,10 +440,25 @@ fun AddEarnings(
 
                     Spacer(modifier = Modifier.width(8.dp))
 
+
+
+
                     Button(
-                        modifier = Modifier.height(56.dp),
-                        shape = RoundedCornerShape(12.dp),
-                        colors = ButtonDefaults.buttonColors(containerColor = backgroundColor),
+//                        onClick = { /* your logic */ },
+                        interactionSource = interactionSource,
+                        modifier = Modifier
+                            .height(56.dp)
+                            .fillMaxWidth()
+                            .graphicsLayer {
+                                scaleX = scale
+                                scaleY = scale
+                            },
+                        shape = RoundedCornerShape(14.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = backgroundColor
+                        )
+
+                    ,
                         onClick = {
 
                             val earned = amountEarned.toDoubleOrNull()
@@ -481,7 +513,11 @@ fun AddEarnings(
                             }
                         }
                     ) {
-                        Text("Save")
+                        Text(
+                            text = "Add",
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold
+                        )
                     }
                 }
             }
